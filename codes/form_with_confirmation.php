db2c86f5651747646ce2ce595f6f97c1c3833d1d
Murilo Michelon <murilomichelon@hotmail.com>
2017-12-20 13:34:12 -0200

    Create form_with_confirmation.php

---
+++ codes/form_with_confirmation.php
@@ -0,0 +1,4 @@
+<form action="http://url.php/page.php" method="post" enctype="multipart/form-data" name="0.1_form1" target="_blank" onsubmit="return window.confirm(" you="" are="" submitting="" information="" to="" an="" external="" page.="" sure?");"="">
+  <input type="file" name="xml">
+  <input type="submit" name="button" value="Enviar">
+</form>
