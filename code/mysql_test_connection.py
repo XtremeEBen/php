6d0aeaf7f66a8ced6f132b44ffa34f0b6a645c88
Murilo Michelon <murilomichelon@hotmail.com>
2017-12-21 11:32:58 -0200

    Delete mysql_test_connection.py

--- code/mysql_test_connection.py
+++
@@ -1,9 +0,0 @@
-<?php 
-	$link = mysqli_connect('localhost', 'root', 'pwd'); 
-	if (!$link)
-	{ 
-		die('Could not connect to MySQL: ' . mysql_error()); 
-	} 
-	echo 'Connection OK';
-	mysqli_close($link); 
-?> 
