2e8c8039e06c9a8b0cf5efa1d61fc7927235dfd4
Murilo Michelon <murilomichelon@hotmail.com>
2017-12-27 15:27:44 -0200

    Update and rename hide_php_name_url.php to htaccess_hide_php_name_url.php

--- codes/htaccess_hide_php_name_url.php
+++ codes/htaccess_hide_php_name_url.php
@@ -1,10 +1,7 @@
 This code is required to be added on the .htaccess, additionally is necessary to be structured
 one controller system to redirect the request to the specific file.
 
-
 Options +FollowSymLinks
 RewriteEngine On
 
 RewriteRule ^inicio?$ index.php
-
-arquivo htaccess
