07d8de098835e8c74366f5720b62f1aac082a6fd
Murilo Michelon <murilomichelon@hotmail.com>
2017-12-27 15:26:59 -0200

    Update switch_structure.php

--- codes/switch_structure.php
+++ codes/switch_structure.php
@@ -1,41 +1,41 @@
 switch ($doc_type)
 {
-  case 'content' :
-  include_once('includes/functions/loader.inc');
-  if ($class == 'index')
-  {
-    if (!empty($si) && ($si->default_home_page == 'content'))
+  case 'content':
+    include_once('includes/functions/loader.inc');
+    if ($class == 'index')
     {
-      include_once THEME_DIR . '/home_page_template.php';
-    }
-    else if (!empty($si) && !empty(($si->default_home_page)))
-    {
-      include_once THEME_DIR . $si->default_home_page;
+      if (!empty($si) && ($si->default_home_page == 'content'))
+      {
+        include_once THEME_DIR . '/home_page_template.php';
+      }
+      else if (!empty($si) && !empty(($si->default_home_page)))
+      {
+        include_once THEME_DIR . $si->default_home_page;
+      }
+      else
+      {
+        header('location: form.php?class_name=user_dashboard_v');
+      }
     }
     else
     {
-      header('location: form.php?class_name=user_dashboard_v');
+      include_once 'content.php';
     }
-  }
-  else
-  {
+    break;
+
+  case 'product':
+    include_once 'product.php';
+    break;
+
+  case 'form':
+    include_once 'form.php';
+    break;
+
+  case 'pform':
+    include_once __DIR__.'/public/public_form.php';
+    break;
+
+  default:
     include_once 'content.php';
-  }
-  break;
-
-  case 'product' :
-  include_once 'product.php';
-  break;
-
-  case 'form' :
-  include_once 'form.php';
-  break;
-
-  case 'pform' :
-  include_once __DIR__.'/public/public_form.php';
-  break;
-
-  default :
-  include_once 'content.php';
-  break;
+    break;
 }
